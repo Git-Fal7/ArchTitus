51daa3405aff01792bd354d4080c580643b09e34
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-01-21 21:09:13 +0300

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -154,6 +154,8 @@ fi
 cp /root/ArchTitus/files/lightdm.conf /etc/lightdm/
 cp /root/ArchTitus/files/lightdm-webkit2-greeter.conf /etc/lightdm/
 cp -r /root/ArchTitus/files/litarvan2 /usr/share/lightdm-webkit/themes/
+git clone https://github.com/ubuntu-cinnamon-remix/yaru-cinnamon
+cp -r /root/yaru-cinnamon/usr/share/ /usr/
 
 
 echo -ne "
